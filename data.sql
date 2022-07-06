613b378188e7fdbc83f6b073aad512ebccd8c6fe
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-07-06 12:59:04 -0500

    Insert and modify data

--- data.sql
+++ data.sql
@@ -16,3 +16,27 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 (10, 'Blossom', '1998-10-13', 3, true, 17),
 (11, 'Ditto', '2022-05-14', 4, true, 22)
 ;
+
+
+INSERT INTO owners (full_name, age) VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38)
+;
+
+INSERT INTO species (name) VALUES
+('Pokemon'),
+('Digimon')
+;
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name LIKE '%mon';
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name in ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name in ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name in ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name in ('Angemon', 'Boarmon');
